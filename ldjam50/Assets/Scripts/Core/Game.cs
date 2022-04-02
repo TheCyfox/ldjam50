ef48fe627f174fbbc7ffa001ce6c69236ac23681
XpArKeR <xparker@gmx.net>
2022-04-02 09:51:54 +0200

    Added some basic stuff

---
+++ ldjam50/Assets/Scripts/Core/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Core
+{
+    public class Game : GameFrame.Core.Game<GameState, PlayerOptions>
+    {
+        protected override GameState InitializeGameState()
+        {
+            return new GameState();
+        }
+
+        protected override PlayerOptions InitialzePlayerOptions()
+        {
+            return new PlayerOptions();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void GameStart()
+        {
+            Base.Core.Game.Startup();
+        }
+    }
+}
